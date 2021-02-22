ae15887005c9d55bb4a472ca6ea04fb70b9d44e9
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2021-02-22 11:03:43 -0500

    finish with adding a todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Fragment } from "react";
+import InputTodo from "./components/InputTodo";
 
 function App() {
   return (
     <Fragment>
       <h1>Hello World</h1>
+      <InputTodo />
     </Fragment>
   );
 }
